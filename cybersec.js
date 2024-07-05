efeabafb045761c9acadc5aded4c129113a8be7f
Alvaro Miranda <varitomiranda@gmail.com>
2024-07-05 00:42:00 -0600

    nodejs adding

--- cybersec.js
+++ cybersec.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use('/', express.static(__dirname + '/'));
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
